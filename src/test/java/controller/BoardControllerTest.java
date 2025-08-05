dbf1859f583c815a2915eae6a36f05158463dfdf
jjuchan <yjc7241@naver.com>
2025-08-05 21:59:02 +0900

    test: board id test add

--- src/test/java/controller/BoardControllerTest.java
+++ src/test/java/controller/BoardControllerTest.java
@@ -38,4 +38,29 @@ class BoardControllerTest {
         String output = testOut.toString();
         assertThat(output).contains("제목:").contains("내용:").contains("작성자:");
     }
+
+    @Test
+    @DisplayName("actionWrtite boardnumber test")
+    void actionWriteBoardNumber() {
+        String input = "테스트 제목\n테스트 내용\n테스트 작성자\n";
+        ByteArrayInputStream testIn = new ByteArrayInputStream(input.getBytes());
+        System.setIn(testIn);
+
+
+        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+
+
+        Scanner scanner = new Scanner(System.in);
+        AppContext context = new AppContext();
+        BoardController controller = new BoardController(context);
+
+
+        controller.actionWrite();
+
+
+        String output = testOut.toString();
+        assertThat(output).contains("1번 글이 작성되었습니다.");
+
+    }
 }
