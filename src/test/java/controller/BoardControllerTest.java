9cb3e2e5f1b8099a4e93f2f61b071c4b5e679384
jjuchan <yjc7241@naver.com>
2025-08-05 22:03:23 +0900

    test: boards id test add

--- src/test/java/controller/BoardControllerTest.java
+++ src/test/java/controller/BoardControllerTest.java
@@ -39,8 +39,9 @@ class BoardControllerTest {
         assertThat(output).contains("제목:").contains("내용:").contains("작성자:");
     }
 
+
     @Test
-    @DisplayName("actionWrtite boardnumber test")
+    @DisplayName("board id test")
     void actionWriteBoardNumber() {
         String input = "테스트 제목\n테스트 내용\n테스트 작성자\n";
         ByteArrayInputStream testIn = new ByteArrayInputStream(input.getBytes());
@@ -60,7 +61,40 @@ class BoardControllerTest {
 
 
         String output = testOut.toString();
-        assertThat(output).contains("1번 글이 작성되었습니다.");
+        assertThat(output).contains("번 글이 작성되었습니다.");
 
     }
+
+    @Test
+    @DisplayName("boards id test")
+    void actionWriteBoardNumbers() {
+        String input = "테스트 제목\n테스트 내용\n테스트 작성자\n" +
+                "테스트 제목2\n테스트 내용2\n테스트 작성자2\n" +
+                "테스트 제목3\n테스트 내용3\n테스트 작성자3\n";
+
+        ByteArrayInputStream testIn = new ByteArrayInputStream(input.getBytes());
+        System.setIn(testIn);
+
+
+        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+
+
+        Scanner scanner = new Scanner(System.in);
+        AppContext context = new AppContext();
+        BoardController controller = new BoardController(context);
+
+
+        controller.actionWrite();
+        controller.actionWrite();
+        controller.actionWrite();
+
+
+        String output = testOut.toString();
+        assertThat(output).contains("1번 글이 작성되었습니다.");
+        assertThat(output).contains("2번 글이 작성되었습니다.");
+        assertThat(output).contains("3번 글이 작성되었습니다.");
+
+    }
+
 }
