afe14619c9bee11f290bc20a2e1fa5c012c32ece
jjuchan <yjc7241@naver.com>
2025-08-06 09:01:46 +0900

    fix: BoardService write method remove setCreatetime, setModifytime

--- src/main/java/board/service/BoardService.java
+++ src/main/java/board/service/BoardService.java
@@ -3,7 +3,6 @@ package board.service;
 import board.entity.Board;
 import board.repository.BoardRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class BoardService {
@@ -13,19 +12,14 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
-
     public Board write(String title, String content, String author) {
         Board board = new Board();
-        board.setId(0);  // 저장 시 save()에서 다시 세팅됨
+        board.setId(0);  // ID는 저장 시 자동으로 증가되므로 초기값은 0으로 설정
         board.setTitle(title);
         board.setAuthor(author);
         board.setContent(content);
         board.setKeyword("");
         board.setViewCount(0);
-
-        LocalDateTime now = LocalDateTime.now();
-        board.setCreatedDate(now);
-        board.setModifiedDate(now);
 
         return boardRepository.save(board);
     }
@@ -37,6 +31,7 @@ public class BoardService {
     public Board findById(int boarId) {
         return boardRepository.findById(boarId);
     }
+
     public void increaseViewCount(int boardId) {
         boardRepository.increaseViewCount(boardId);
     }
